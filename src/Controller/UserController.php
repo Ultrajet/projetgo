1d6af7340997caf6baec5374ba781f287e9a51d4
Ultrajet <51157704+Ultrajet@users.noreply.github.com>
2019-10-09 14:46:52 +0200

    index dans UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/", name="index")
      */
     public function index()
     {
