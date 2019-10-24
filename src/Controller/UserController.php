a8c8a046314af99cb6eb956862f3fe3dcfb67a27
Ultrajet <51157704+Ultrajet@users.noreply.github.com>
2019-10-24 16:10:21 +0200

    profils

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,10 +20,23 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/profil", name="profil")
+     * @Route("/profil/{id}", name="profil")
      */
-    public function profil()
+    public function profil($id)
     {
-        return $this->render('user/profil.html.twig');
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->find($id);
+
+        return $this->render('user/profil.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/profil/modifier", name="modifier_profil")
+     */
+    public function modifierProfil()
+    {
+        return $this->render('user/modifier_profil.html.twig');
     }
 }
