e0343b67e1af75e47f362f8d4c96df7cf1033a60
Ultrajet <51157704+Ultrajet@users.noreply.github.com>
2019-10-29 17:04:15 +0100

    formulaires are fun

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -19,23 +21,40 @@ class UserController extends AbstractController
     }
 
     /**
+     * @Route("/profil/modifier", name="modifierProfil")
+     */
+    public function modifierProfil(Request $request)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted()) {
+            dump($form->getViewData());
+            exit;
+        }
+
+        return $this->render('user/modifier_profil.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
      * @Route("/profil/{id}", name="profil")
      */
     public function profil($id)
     {
         $repository = $this->getDoctrine()->getRepository(User::class);
+
+        $thisUserId = $this->getUser()->getId();
+        $owner = $thisUserId == $id ? true : false;
+
         $user = $repository->find($id);
 
         return $this->render('user/profil.html.twig', [
-            'user' => $user
+            'user' => $user,
+            'owner' => $owner
         ]);
-    }
-
-    /**
-     * @Route("/profil/modifier", name="modifier_profil")
-     */
-    public function modifierProfil()
-    {
-        return $this->render('user/modifier_profil.html.twig');
     }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Jeu;
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('email')
+            ->add('ville')
+            ->add('txt_profil')
+            // ->add('jeux', EntityType::class, [
+            //     'mapped' => false,
+            //     'class' => Jeu::class,
+            //     'choice_label' => 'nom',
+            //     'multiple' => true,
+            //     'expanded' => true,
+            // ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
